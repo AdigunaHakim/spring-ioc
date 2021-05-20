ad4ede5386fcc62ea1f796bd00ae5afdbc17b5ff
Adiguna Hakim <adiguna.hakim@btpn.com>
2021-05-20 15:44:11 +0700

    add some properties on student class

--- src/main/java/com/example/Student.java
+++ src/main/java/com/example/Student.java
@@ -1,13 +1,36 @@
 package com.example;
 
 public class Student {
+    private int id;
+    private String name;
     private MathCheat mathCheat;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MathCheat getMathCheat() {
+        return mathCheat;
+    }
 
     public void setMathCheat(MathCheat mathCheat) {
         this.mathCheat = mathCheat;
     }
 
     public void cheating(){
+        System.out.println("Student id : " + id + " name : " + name);
         mathCheat.doCheat();
     }
 }
